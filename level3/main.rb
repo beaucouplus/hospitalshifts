7f0ec856d7f110e091112157b344e24d42b05f05
beaucouplus <maxime@beaucouplus.com>
2018-01-28 19:45:55 +0100

    solved exercice 3

---
+++ level3/main.rb
@@ -0,0 +1,34 @@
+require "json"
+require "pp"
+require 'time'
+
+# your code
+file = File.read("data.json")
+data = JSON.parse(file)
+shifts = data["shifts"]
+workers = data["workers"]
+status = { medic: 270, interne: 126 }
+
+def define_shift_fee(shifts,worker)
+  worker_shifts = shifts.select { |shift| shift["user_id"] == worker["id"] }
+  shift_fee = worker_shifts.count
+  double_pay_shifts = worker_shifts.select do |shift|
+    shift_time = Time.parse(shift["start_date"]).wday
+    # [0,6] = positions of sunday and saturday in wday array
+    [0,6].include?(shift_time)
+  end.count
+  shift_fee += double_pay_shifts
+end
+
+
+night_shift = workers.each_with_object({"workers" => []}) do |worker, night_shift|
+  shift_number = define_shift_fee(shifts,worker)
+  total_price = shift_number * status[worker["status"].to_sym]
+  night_shift["workers"] << { "id": worker["id"], "price": total_price }
+end
+
+
+
+
+# created a new json file in order to leave the example file.
+File.open("night_shifts.json", 'w+'){ |file| file.write(night_shift.to_json)}
